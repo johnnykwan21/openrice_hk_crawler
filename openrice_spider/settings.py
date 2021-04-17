da7b13d4b8ce7585e2e21d287b71ca3bb5a5ec16
Johnny Kwan <johnnykwan2121@gmail.com>
2021-04-17 20:33:20 +0800

    Update settings.py

--- openrice_spider/settings.py
+++ openrice_spider/settings.py
@@ -29,23 +29,23 @@ ITEM_PIPELINES = {
     'openrice.pipelines.OpenriceRestaurantPipeline': 300,
 }
 
-# Proxy List
-DOWNLOADER_MIDDLEWARES = {
-    'rotating_proxies.middlewares.RotatingProxyMiddleware': 610,
-    'rotating_proxies.middlewares.BanDetectionMiddleware': 620,
-}
-ROTATING_PROXY_LIST_PATH = 'proxy_list.txt'
+# # Proxy List
+# DOWNLOADER_MIDDLEWARES = {
+#     'rotating_proxies.middlewares.RotatingProxyMiddleware': 610,
+#     'rotating_proxies.middlewares.BanDetectionMiddleware': 620,
+# }
+# ROTATING_PROXY_LIST_PATH = 'proxy_list.txt'
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 300
+CONCURRENT_REQUESTS = 64
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
 DOWNLOAD_DELAY = 0
 # The download delay setting will honor only one of:
-CONCURRENT_REQUESTS_PER_DOMAIN = 80
-CONCURRENT_REQUESTS_PER_IP = 80
+CONCURRENT_REQUESTS_PER_DOMAIN = 32
+CONCURRENT_REQUESTS_PER_IP = 32
 
 # Disable cookies (enabled by default)
 #COOKIES_ENABLED = False
